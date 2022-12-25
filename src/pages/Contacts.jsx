3ee98e8cc03bd52672c1fc169b73ab0ba68580d9
Tomasz Skolimo <skolim10@gmail.com>
2022-12-25 18:49:36 +0100

    pagesUpdate2

---
+++ src/pages/Contacts.jsx
@@ -0,0 +1,64 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect } from 'react';
+import { fetchContacts } from 'redux/contacts/operations';
+import ContactPhoneIcon from '@mui/icons-material/ContactPhone';
+import ImportContactsIcon from '@mui/icons-material/ImportContacts';
+import { ContactList } from 'components/ContactList/ContactList';
+import { Filter } from 'components/Filter/Filter';
+import { selectError, selectIsLoading } from 'redux/contacts/selector';
+import { ContactForm } from 'components/ContactForm/ContactForm';
+import { Loader } from 'components/Loader/Loader';
+
+export default function Contacts() {
+  const dispatch = useDispatch();
+  const isLoading = useSelector(selectIsLoading);
+  const error = useSelector(selectError);
+
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
+
+  return (
+    <div
+      style={{
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        marginTop: 50,
+        fontSize: 24,
+        color: '#010101',
+      }}
+    >
+      <h1
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          color: '#770e7e',
+          textShadow: '3px 2px 3px rgba(119, 14, 126, 0.2)',
+        }}
+      >
+        <ImportContactsIcon sx={{ fontSize: 70 }} />
+        Phonebook
+      </h1>
+      <ContactForm />
+
+      <h2
+        style={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 8,
+          color: '#770e7e',
+          textShadow: '3px 2px 3px rgba(119, 14, 126, 0.2)',
+        }}
+      >
+        <ContactPhoneIcon sx={{ fontSize: 40 }} />
+        Contacts
+      </h2>
+      <Filter />
+      {isLoading && !error && <Loader />}
+
+      <ContactList />
+    </div>
+  );
+}
---
+++ src/pages/Home.jsx
@@ -0,0 +1,24 @@
+const styles = {
+  container: {
+    minHeight: 'calc(100vh - 50px)',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 500,
+    fontSize: 54,
+    textAlign: 'center',
+  },
+};
+
+export default function Home() {
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>
+        Contacts manager <br />
+        welcome page <br />
+      </h1>
+    </div>
+  );
+}
